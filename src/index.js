3a77706b21b39f6b1a14c326fe055b9955da2455
Manoj Nama <manoj.nama@outlook.com>
2020-05-25 10:09:36 +1000

    Updating imports, adding getManager utility

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-const Manager = require('./manager');
-exports.TransactionManager = require('./manager/context-manager');
+const Manager = require('./transaction-manager');
+const { getManager: getTransactionManager } = require('./transaction-manager/manager')
+const { getRequestContext } = require('./request-context');
 
+exports.TransactionManager = require('./transaction-manager/manager');
 exports.init = (options) => {
   Manager.init(options);
+};
+exports.getManager = (identifier) => {
+  const context = getRequestContext(identifier);
+  if (context) {
+    const transactionId = context.get('id');
+    return getTransactionManager(transactionId);
+  }
 };
